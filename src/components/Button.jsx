7d979181e7dc4621d65b1bb1eb661547784c7f01
Nada Alarbeed <nadaalarbeed04@gmail.com>
2025-12-30 21:55:40 +0300

    edit the props

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,8 +1,8 @@
 import { FaShoppingCart } from "react-icons/fa"
 
-const Button = ({ text, icon = false }) => {
+const Button = ({ text, icon = false,type = "button",onClick  }) => {
   return (
-    <button className="flex justify-center items-center cursor-pointer bg-yellow text-white dark:text-black text-[1rem] rounded-full duration-300 hover:bg-yellowForHover flex-nowrap">
+    <button  type={type}  onClick={onClick} className="flex justify-center items-center cursor-pointer bg-yellow text-white dark:text-black text-[1rem] rounded-full duration-300 hover:bg-yellowForHover flex-nowrap">
       <span className="px-10 py-2 flex items-center">
         {text}
         {icon && 
