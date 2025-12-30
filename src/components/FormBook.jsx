c9ff6196575bb66fa3a46c68653481042b01fa6e
Nada Alarbeed <nadaalarbeed04@gmail.com>
2025-12-30 21:55:18 +0300

    edit the props

--- src/components/FormBook.jsx
+++ src/components/FormBook.jsx
@@ -1,9 +1,10 @@
-const FormBook = ({ placeholder, type = "text", options = [] }) => {
-  const baseClasses = "w-full h-12.5 pl-6.25 mb-6.25 border border-[#999] p-3 rounded-[5px] text-[1.1rem] placeholder:text-gray dark:text-white dark:placeholder:text-white input";
+const FormBook = ({ placeholder, type = "text", options = [], autoFocus,readOnly,required,name, autoComplete = "on" }) => {
+  const baseClasses = "w-full h-12.5 pl-6.25 mb-6.25 border border-[#999] p-3 rounded-[5px] text-[1.1rem] placeholder:text-gray dark:text-white dark:placeholder:text-white input ";
   
   if (type === "select") {
     return (
-      <select className={`${baseClasses}  input `} defaultValue=""  style={{ colorScheme: "dark" }}>
+      <select className={`${baseClasses}  input `} defaultValue=""  style={{ colorScheme: "dark" }}  required={required}  name={name}
+        autoComplete={autoComplete}>
         <option className="dark:bg-gray" value="" disabled>{placeholder}</option>
         {options.map((option, index) => (
           <option className="dark:bg-gray" key={index} value={option.value}>{option.label}</option>
@@ -19,6 +20,11 @@ const FormBook = ({ placeholder, type = "text", options = [] }) => {
         type="date" 
         className={`${baseClasses} pr-10 dark:scheme-dark`}
         placeholder={placeholder}
+          required={required}
+        readOnly={readOnly}
+        autoFocus={autoFocus}
+         name={name}
+        autoComplete={autoComplete}
       />
     );
   }
@@ -28,6 +34,12 @@ const FormBook = ({ placeholder, type = "text", options = [] }) => {
       type={type}
       placeholder={placeholder}
       className={baseClasses}
+        autoFocus={autoFocus}
+      readOnly={readOnly}
+      required={required}
+       name={name}
+        autoComplete={autoComplete}
+      
     />
   );
 }
